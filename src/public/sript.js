95152357f3f7101a0df6af1c2782af5cf5e8f175
Magdy371 <magdyelshrief37@gmail.com>
2025-09-02 07:27:40 +0300

    ph1.1

--- src/public/sript.js
+++ src/public/sript.js
@@ -2,7 +2,7 @@ const noteForm = document.getElementById("noteForm");
 const notesDiv = document.getElementById("notes");
 
 async function fetchNotes() {
-  const res = await fetch("/api/notes");
+  const res = await fetch("/api");
   const notes = await res.json();
   notesDiv.innerHTML = notes
     .map(
@@ -22,7 +22,7 @@ noteForm.addEventListener("submit", async (e) => {
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
 
-  await fetch("/api/notes", {
+  await fetch("/api", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ title, content }),
